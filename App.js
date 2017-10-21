32234ea5ebd5a069433b03be4d44e03f5de5a118
ammarRBK <alshakmaskeain@gmail.com>
2017-10-21 15:31:09 +0300

    start routing

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, AppRegistry, Image, KeyboardAvoidingView} from 'react-native';
+import { StyleSheet, Text, AppRegistry, Image, KeyboardAvoidingView, TouchableOpacity} from 'react-native';
 import List from './client/eventstodo';
 import Login from './client/login';
 
@@ -7,22 +7,39 @@ export default class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      showLogIn: false
+      showSigninUser: false,
+      showSigninOrg: false,
+      showSignupUser: false,
+      showSignupOrg: false,
+      showEvents: false,
+      showCorrentEvent:false,
+      showUserProfile:false,
+      showOrgProfile:false
     };
   }
+  listComp =() => {
+    this.setState({ showEvents: true });
+    this.setState({ showSignin: false });
+  };
   userLogin = () => {
-    this.setState({ showLogIn: true });
+    this.setState({ showSignin: true });
+    this.setState({ showEvents: false });
   };
   render() {
     return (
       <KeyboardAvoidingView behavior='padding' style={styles.container}>
-        <Text>ammar</Text>
-        <Text></Text>
         <Image source={require('./images/32799248.png')}/>
-        <Login />
-        <Text>Fill your infromations and press sign in</Text>
-        <Text>to see your profile or the events.</Text>
-        <List/>
+        <Text style={{fontFamily:'Signboard'}}>Wlcom to our applecation ['Humanity'] If you are from our family</Text>
+        <Text style={{fontFamily:'Signboard'}}> and you want to signIn WELCOM from here press on signIn</Text>
+        <TouchableOpacity onPress={this.userLogin}>
+          <Text>Login</Text>
+        </TouchableOpacity>
+        {this.state.showLogIn ? <Login /> : null}
+        <Text style={{fontFamily:'Signboard'}}> Or if you want to JOIN US WELCOM from here</Text>
+        <TouchableOpacity onPress={this.listComp}>
+          <Text>Sign up</Text>
+        </TouchableOpacity>
+        {this.state.showEvents ? <List /> : null}
       </KeyboardAvoidingView>
     );
   }
